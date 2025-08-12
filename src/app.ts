fbe8bc4304de8f86dc2dfdd7eed9e79d62f0fc7f
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-08-11 21:29:00 -0300

    login e cadastro pronto

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { handleErrors } from "./errors"
 
 import cors from "cors"
 import { loginRoutes } from './Routes/login.routes'
+import { usuariosRoutes } from './Routes/usuarios.routes'
 
 
 const app:Application = express()
@@ -12,7 +13,7 @@ const app:Application = express()
 app.use(cors())
 app.use(express.json())
 app.use("/login", loginRoutes )
-
+app.use("/usuarios", usuariosRoutes)
 
 app.use(handleErrors)
 export default app
