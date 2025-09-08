550fa975e9f0ac9d1364e7cd9df314728b120f20
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-08 04:27:02 -0300

    alwaysdata banco

--- src/data-source.ts
+++ src/data-source.ts
@@ -28,5 +28,6 @@ const dataSourceConfig = (): DataSourceOptions => {
 };
 
 const AppDataSource = new DataSource(dataSourceConfig());
-
+//npm run typeorm migration:generate ./src/migrations/InitialMigration -- -d ./src/data-source.ts
+//npm run typeorm migration:run -- -d ./src/data-source
 export { AppDataSource };
