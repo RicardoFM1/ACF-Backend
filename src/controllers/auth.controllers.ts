93708d0faacfe2f3054cd63b1e1594529c27bdcd
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-08 19:05:25 -0300

    auth modification

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -3,21 +3,14 @@ import { ResetPasswordService } from "../services/User/resetPassword.service";
 import { Request, Response } from "express"
 
 export const forgotPasswordController = async (req: Request, res: Response) => {
-  try {
     const { email } = req.body;
     await ForgotPasswordService(email); 
     return res.status(200).json({ message: "Link de redefinição enviado por email." });
-  } catch (err: any) {
-    return res.status(err.statusCode || 400).json({ message: err.message || "Erro ao enviar email" });
-  }
+  
 };
 export const resetPasswordController = async (req:Request, res:Response) => {
-
-  try {
     const { token, novaSenha } = req.body;
     const result = await ResetPasswordService(token, novaSenha);
     return res.status(200).json({message: "Senha redefinida com sucesso!"});
-  } catch (err: any) {
-    return res.status(err.statusCode || 500).json({ message: err.message });
-  }
+  
 }
